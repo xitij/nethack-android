c2e84eb12836b27355e5e518a61970b1dac018cf
astaroth <astaroth@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2009-09-15 06:47:59 +0000

    Updated setup script.

--- trunk/nethack-3.4.3/sys/android/setup.sh
+++ trunk/nethack-3.4.3/sys/android/setup.sh
@@ -7,3 +7,13 @@ cp Makefile.dat ../../dat/Makefile
 cp Makefile.doc ../../doc/Makefile
 cp Makefile.src ../../src/Makefile
 cp Makefile.utl ../../util/Makefile
+
+echo "Setting absolute path"
+
+sed $(echo s@ABSOLUTE_PATH@$(pwd)@g) < NetHackNative/Application0.mk > NetHackNative/Application.mk
+
+echo "Setting up links"
+
+cd NetHackApp
+ln -s ../NetHackNative/libs
+cd ..
