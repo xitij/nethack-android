d92e8055c82d9ca3ee5ed0193be86b58091e6fa2
astaroth <astaroth@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2009-09-17 07:52:27 +0000

    Some temp interface code.

--- nethack-3.4.3/sys/android/NetHackNative/nethack/javainterface.c
+++ nethack-3.4.3/sys/android/NetHackNative/nethack/javainterface.c
@@ -5,6 +5,11 @@
 #include <unistd.h>
 #include <pthread.h>
 
+
+static char *s_MainViewText;
+static int s_MainViewNumCols;
+static int s_MainViewNumRows;
+
 int g_Test1 = 42;
 
 #if 0
@@ -12,13 +17,52 @@ int g_Test1 = 42;
                           void *(*start_routine) (void *), void *arg);
 #endif
 
+static int s_MainViewX = 0;
+static int s_MainViewY = 0;
+
+static int s_MainViewChanged = 0;
+
+void xputc(int c)
+{
+	if(s_MainViewX < s_MainViewNumCols && s_MainViewY < s_MainViewNumRows)
+	{
+		s_MainViewText[s_MainViewNumCols*s_MainViewY + s_MainViewX] = (char)c;
+	}
+
+	s_MainViewX++;
+	if(s_MainViewX >= s_MainViewNumCols)
+	{
+		s_MainViewX = 0;
+		s_MainViewY++;
+	}
+
+	s_MainViewChanged = 1;
+}
+
+
+void xputs(const char *s)
+{
+	const char *ptr = s;
+	while(*s)
+	{
+		xputc((int)(*s++));
+	}
+}
+
 pthread_t g_ThreadHandle;
 
 static void *sThreadFunc()
 {
-#if 0
-	while(g_Test1 <= 1000)
-#endif
+/*
+	xputc('');
+	xputc('B');
+*/
+	char buff[256];
+
+	sprintf(buff, "%d x %d", s_MainViewNumCols, s_MainViewNumRows);
+	xputs(buff);
+
+
 	while(1)
 	{
 		g_Test1++;
@@ -28,13 +72,42 @@ static void *sThreadFunc()
 }
 
 
-int Java_com_nethackff_NetHackApp_TestInit(JNIEnv *env, jobject thiz)
+int Java_com_nethackff_NetHackApp_TestInit(JNIEnv *env, jobject thiz,
+		int numcols, int numrows)
 {
+	char *p;
+	int x, y;
+
 	if(g_ThreadHandle)
 	{
 		return 0;
 	}
 
+	if(s_MainViewText)
+	{
+		return 0;
+	}
+
+	s_MainViewNumCols = numcols;
+	s_MainViewNumRows = numrows;
+
+	s_MainViewText = malloc(s_MainViewNumCols*s_MainViewNumRows + 1);
+	s_MainViewText[s_MainViewNumCols*s_MainViewNumRows] = '\0';
+
+	p = s_MainViewText;
+	for(y = 0; y < s_MainViewNumRows; y++)
+	{
+		for(x = 0; x < s_MainViewNumCols; x++)
+		{
+			*p++ = ' ';
+		}
+	}
+
+
+/*
+	sprintf(s_MainViewText, "123456789012345\n6");
+	sprintf(s_MainViewText + s_MainViewNumCols, "67890");
+*/
 	g_Test1 = 25;
 	printf("??? TEST-- What happens to this?\n");
 
@@ -55,6 +128,12 @@ void Java_com_nethackff_NetHackApp_TestShutdown(JNIEnv *env, jobject thiz)
 		pthread_cancel(g_ThreadHandle);	/* Would return 0 on success. */
 #endif
 		g_ThreadHandle = 0;
+	}
+
+	if(s_MainViewText)
+	{
+		free(s_MainViewText);
+		s_MainViewText = NULL;
 	}
 }
 
@@ -79,5 +158,5 @@ Java_com_nethackff_NetHackApp_stringFromJNI( JNIEnv* env,
 	g_Test1++;
     return (*env)->NewStringUTF(env, "Hello from JNI !");
 #endif
-    return (*env)->NewStringUTF(env, buff);
+    return (*env)->NewStringUTF(env, s_MainViewText);
 }
