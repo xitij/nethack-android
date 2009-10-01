2bbf2fdff0e867fcec76eb5261c596750af67d9d
astaroth <astaroth@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2009-10-01 07:11:24 +0000

    Some more work on terminal emulation code, making at least some of the formatting correct. The game map is still a bit messed up.

--- nethack-3.4.3/sys/android/NetHackApp/src/com/nethackff/NetHackApp.java
+++ nethack-3.4.3/sys/android/NetHackApp/src/com/nethackff/NetHackApp.java
@@ -36,6 +36,49 @@ class TerminalView extends View
 	int currentRow;
 	int currentColumn;
 
+	void clearScreen()
+	{
+		for(int i = 0; i < numRows*numColumns; i++)
+		{
+			textBuffer[i] = ' ';
+		}
+	}
+
+	void clampCursorPos()
+	{
+		if(currentRow < 0)
+		{
+			currentRow = 0;
+		}
+		else if(currentRow >= numRows)
+		{
+			// Should we scroll down in this case?
+			currentRow = numRows - 1;
+		}
+		if(currentColumn < 0)
+		{
+			currentColumn = 0;
+		}
+		else if(currentColumn >= numColumns)
+		{
+			currentColumn = numColumns - 1;
+		}
+	}
+	
+	void moveCursorRel(int coldelta, int rowdelta)
+	{
+		currentRow += rowdelta;
+		currentColumn += coldelta;
+		clampCursorPos();
+	}
+	
+	void moveCursorAbs(int newcol, int newrow)
+	{
+		currentRow = newrow;
+		currentColumn = newcol;
+		clampCursorPos();
+	}
+
 	public void lineFeed()
 	{
 		currentRow++;
@@ -72,6 +115,22 @@ class TerminalView extends View
 		currentColumn++;
 	}
 
+	public void setCharAtPos(char c, int col, int row)
+	{
+		if(col >= 0 && col < numColumns && row >= 0 && row < numRows)
+		{
+			textBuffer[row*numColumns + col] = c;
+		}
+	}
+	public void writeRawStr(String s)
+	{
+		int len = s.length();
+		for(int i = 0; i < len; i++)
+		{
+			writeRaw(s.charAt(i));
+		}
+	}
+
 	private static final int ESC_NONE = 0;
 	private static final int ESC = 1;
 	private static final int ESC_LEFT_SQUARE_BRACKET = 2;
@@ -80,11 +139,16 @@ class TerminalView extends View
 
 	public void startEscapeSequence(int state)
 	{
+		escSeqLen = 0;
 		escapeState = state;
 	}
 
 	public void updateEscapeSequence(char c)
 	{
+		if(escSeqLen < kMaxEscSeqLen)
+		{
+			escSeqStored[escSeqLen++] = c;
+		}
 		switch(escapeState)
 		{
 			case ESC:
@@ -96,7 +160,7 @@ class TerminalView extends View
 				break;
 
 			default:
-				// TODO
+				reportUnknownSequence();
 				escapeState = ESC_NONE;
 				break;
 		}
@@ -112,6 +176,7 @@ class TerminalView extends View
 				break;
 
 			default:
+				reportUnknownSequence();
 				escapeState = ESC_NONE;
 				break;
 		}
@@ -121,17 +186,115 @@ class TerminalView extends View
 	public int []escSeqArgVal = new int[kMaxEscParam];
 	public int escSeqArgCnt = 0;
 
+	public static final int kMaxEscSeqLen = 64;	// Not sure...
+	public char []escSeqStored = new char[kMaxEscSeqLen];
+	public int escSeqLen = 0;
+
+	public int getEscSeqArgVal(int deflt)
+	{
+		if(escSeqArgCnt < 0)
+		{
+			// No arguments specified.
+			return deflt;
+		}
+		else
+		{
+			return escSeqArgVal[escSeqArgCnt];
+		}
+	}
+
+	public void reportUnknownChar(char c)
+	{
+		if(currentColumn > 1)
+		{
+			lineFeed();
+		}
+		writeRawStr("Unknown character: " + (int)c);
+		lineFeed();
+	}
+	
+	public void reportUnknownSequence()
+	{
+		if(currentColumn > 1)
+		{
+			lineFeed();
+		}
+		writeRawStr("Unknown Esc sequence: ");
+		for(int i = 0; i < escSeqLen; i++)
+		{
+			writeRaw(escSeqStored[i]);
+		}
+		lineFeed();
+	}
+	
 	public void updateEscapeSequenceLeftSquareBracket(char c)
 	{
 		switch(c)
 		{
-			case 'B':
+/*
+TODO: [7m
+[0m
+ */
+			case 'B':	// Move cursor down n lines
+				moveCursorRel(0, getEscSeqArgVal(1));
 				escapeState = ESC_NONE;
 				return;
-			case 'D':
+			case 'C':	// Move cursor right n lines 
+				moveCursorRel(getEscSeqArgVal(1), 0);
 				escapeState = ESC_NONE;
 				return;
-			case 'H':	// Cursor Home
+			case 'D':	// Move cursor left n lines 
+				moveCursorRel(getEscSeqArgVal(1), 0);
+				escapeState = ESC_NONE;
+				return;
+			case 'A':	// Move cursor up n lines
+				moveCursorRel(0, -getEscSeqArgVal(1));
+				escapeState = ESC_NONE;
+				return;
+			case 'H':	// Cursor home
+				if(escSeqArgCnt == 1)
+				{
+					moveCursorAbs(escSeqArgVal[1] - 1, escSeqArgVal[0] - 1);
+				}
+				else
+				{
+					moveCursorAbs(0, 0);
+				}
+				escapeState = ESC_NONE;
+				return;
+			case 'J':	// Clear screen
+				// TODO: Read arguments here.
+				clearScreen();
+				escapeState = ESC_NONE;
+				return;
+			case 'K':
+				if(getEscSeqArgVal(0) == 0)
+				{
+					// Clear line from cursor right
+					for(int i = currentColumn; i < numColumns; i++)
+					{
+						setCharAtPos(' ', i, currentRow);
+					}
+				}
+				else if(getEscSeqArgVal(0) == 1)
+				{
+					// Clear line from cursor left
+					for(int i = currentColumn; i >= 0; i--)
+					{
+						setCharAtPos(' ', i, currentRow);
+					}
+				}
+				else if(getEscSeqArgVal(0) == 2)
+				{
+					for(int i = 0; i < numColumns; i++)
+					{
+						setCharAtPos(' ', i, currentRow);
+					}
+				}
+				else
+				{
+					reportUnknownSequence();
+				}
 				escapeState = ESC_NONE;
 				return;
 		}
@@ -147,13 +310,75 @@ class TerminalView extends View
 		{
 			escSeqArgCnt++;
 		}
-		// else error?
-		escapeState = ESC_NONE;
+		else
+		{
+			reportUnknownSequence();
+			escapeState = ESC_NONE;
+		}
 	}
 	public void write(char c)
 	{
 		switch(c)
 		{
+			case 0:	// NUL
+				break;
+			case 7: // BEL
+				break;
+			case 8:	// BS
+				if(currentColumn > 0)
+					currentColumn--;
+				break;
+			case 9:	// HT
+				// TODO
+reportUnknownChar(c);
+				break;
+			case 13:
+				currentColumn = 0;
+				return;
+			case 10:	// CR
+			case 11:	// VT
+			case 12:	// LF
+				lineFeed();
+				return;
+			case 14:	// SO
+				// TODO
+//				break;
+			case 15:	// SI
+				// TODO
+//				break;
+			case 24:	// CAN
+			case 26:	// SUB
+				// TODO
+//				break;
+			case 0x9b:	// CSI
+reportUnknownChar(c);
+				break;
+			case 27:	// ESC
+				startEscapeSequence(ESC);
+				return;
+		};
+
+		if(escapeState == ESC_NONE)
+		{
+			if(c >= 32)
+			{
+				writeRaw(c);
+			}
+			else
+			{
+				// TEMP
+				writeRaw(':');
+				int a = c/10;
+				int b = c - a*10;
+				writeRaw((char)('0' + a));
+				writeRaw((char)('0' + b));
+			}
+		}
+		else
+		{
+			updateEscapeSequence(c);
+		}
+/*
 			case 0:	// NUL
 				break;
 			case 7: // BEL
@@ -182,9 +407,6 @@ class TerminalView extends View
 			case 26:	// SUB
 				// TODO
 				break;
-			case 27:	// ESC
-				startEscapeSequence(ESC);
-				break;
 			case 0x9b:	// CSI
 				break;
 			default:
@@ -201,6 +423,7 @@ class TerminalView extends View
 				}
 				break;
 		}
+*/
 	}
 
 	public void write(String s)
@@ -223,13 +446,13 @@ class TerminalView extends View
 		
 		textBuffer = new char[rows*columns];
 
-		for(int i = 0; i < rows*columns; i++)
-		{
-			textBuffer[i] = ' ';
-		}
+		clearScreen();
 
 		currentRow = 0;
 		currentColumn = 0;
+
+		writeRawStr("TEST TEST");
+		lineFeed();
 	}
 
 	public String getContents()
@@ -260,7 +483,6 @@ class TerminalView extends View
 		Paint paint = new Paint();
 		paint.setARGB(255, 255, 255, 255);
 		paint.setTypeface(Typeface.MONOSPACE);
-//		paint.setTextSize(14);
 		paint.setTextSize(10);
 		paint.setAntiAlias(true);
 		int charheight = (int)Math.ceil(paint.getFontSpacing());// + paint.ascent());
