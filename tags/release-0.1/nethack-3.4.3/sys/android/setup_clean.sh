594b78ffbc7b7570031e669e1abf423ee891354f
astaroth <astaroth@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2009-10-15 08:01:36 +0000

    Integrated changes for SDK 1.6, which is what this release ended up being compiled with.

--- tags/release-0.1/nethack-3.4.3/sys/android/setup_clean.sh
+++ tags/release-0.1/nethack-3.4.3/sys/android/setup_clean.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ANDROID_NDK_DIR=$(echo ~)/android-ndk-1.5_r1
+ANDROID_NDK_DIR=$(echo ~)/android-ndk-1.6_r1
 
 rm NetHackApp/libs
 rm $ANDROID_NDK_DIR/apps/NetHackNative
-rm $ANDROID_NDK_DIR/sources/NetHackNative
+# rm $ANDROID_NDK_DIR/sources/NetHackNative
 rm NetHackNative/nethack
 rm NetHackApp/assets/nethackdir
 rm ../../src/Android.mk
