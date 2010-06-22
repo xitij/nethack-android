28be8b641852650a11a653d036840b35d21b8a94
farnstrom <farnstrom@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2010-06-22 03:37:54 +0000

    Some more work towards eliminating use of external commands which may not exist. Still using an undocumented interface for the chmod replacement, not entirely happy with that.

--- trunk/nethack-3.4.3/sys/android/NetHackApp/src/com/nethackff/NetHackApp.java
+++ trunk/nethack-3.4.3/sys/android/NetHackApp/src/com/nethackff/NetHackApp.java
@@ -49,6 +49,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.Thread;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class NetHackApp extends Activity implements Runnable, OnGestureListener
@@ -669,6 +670,48 @@ public class NetHackApp extends Activity implements Runnable, OnGestureListener
 		return commThreadRunning;
 	}
 
+	public void chmod(String filename, int permissions)
+	{
+		// This was a bit problematic - there is an android.os.FileUtils.setPermissions()
+		// function, but apparently that is not a part of the supported interface.
+		// I found some other options:
+		// - java.io.setReadOnly() exists, but seems limited.
+		// - java.io.File.setWritable() is a part of Java 1.6, but doesn't seem to exist in Android.
+		// - java.nio.file.attribute.PosixFilePermission also doesn't seem to exist under Android.
+		// - doCommand("/system/bin/chmod", permissions, filename) was what I used to do, but it was crashing for some.
+		// I don't think these permissions are actually critical for anything in the application,
+		// so for now, we will try to use the undocumented function and just be careful to catch any exceptions
+		// and print some output spew. /FF
+		
+		try
+		{
+		    Class<?> fileUtils = Class.forName("android.os.FileUtils");
+		    Method setPermissions =
+		        fileUtils.getMethod("setPermissions", String.class, int.class, int.class, int.class);
+		    int a = (Integer) setPermissions.invoke(null, filename, permissions, -1, -1);
+		    if(a != 0)
+		    {
+				Log.i("NetHackDbg", "android.os.FileUtils.setPermissions() returned " + a + " for '" + filename + "', probably didn't work.");
+		    }
+		}
+		catch(ClassNotFoundException e)
+		{
+			Log.i("NetHackDbg", "android.os.FileUtils.setPermissions() failed - ClassNotFoundException.");
+		}
+		catch(IllegalAccessException e)
+		{
+			Log.i("NetHackDbg", "android.os.FileUtils.setPermissions() failed - IllegalAccessException.");
+		}
+		catch(InvocationTargetException e)
+		{
+			Log.i("NetHackDbg", "android.os.FileUtils.setPermissions() failed - InvocationTargetException.");
+		}
+		catch(NoSuchMethodException e)
+		{
+			Log.i("NetHackDbg", "android.os.FileUtils.setPermissions() failed - NoSuchMethodException.");
+		}
+	}
+	
 	public void mkdir(String dirname)
 	{
 		// This is how it used to be done, but it's probably not a good idea
@@ -681,6 +724,9 @@ public class NetHackApp extends Activity implements Runnable, OnGestureListener
 		if(status)
 		{
 			Log.i("NetHackDbg", "Created dir '" + dirname + "'");
+
+			// Probably best to keep stuff accessible, for now.
+			chmod(dirname, 0777);
 		}
 		else
 		{
@@ -905,7 +951,7 @@ public class NetHackApp extends Activity implements Runnable, OnGestureListener
 			for(int i = 0; i < assets.length; i++)
 			{
 				copyAsset("nethackdir/" + assets[i]);
-				doCommand("/system/bin/chmod", "666", "/data/data/com.nethackff/nethackdir/" + assets[i]);
+				chmod("/data/data/com.nethackff/nethackdir/" + assets[i], 0666);
 			}
 		}
 		catch (IOException e)
