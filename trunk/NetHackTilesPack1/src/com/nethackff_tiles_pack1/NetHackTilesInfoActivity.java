dafd34fb2e2a82cda96ada79c1f38497c3edee25
farnstrom <farnstrom@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2010-07-31 22:57:23 +0000

    Numerous improvements to the tile package info screen.

--- trunk/NetHackTilesPack1/src/com/nethackff_tiles_pack1/NetHackTilesInfoActivity.java
+++ trunk/NetHackTilesPack1/src/com/nethackff_tiles_pack1/NetHackTilesInfoActivity.java
@@ -3,7 +3,10 @@ package com.nethackff_tiles_pack1;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.regex.Pattern;
@@ -19,7 +22,7 @@ public class NetHackTilesInfoActivity extends Activity
 		layout.addView(headerview);
 
 		TextView infoview = new TextView(this);
-		infoview.setText(info);
+		infoview.setText(info + "\n");
 		layout.addView(infoview);
 
 		return infoview;
@@ -29,9 +32,17 @@ public class NetHackTilesInfoActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.tilesinfoactivity);
+		setContentView(R.layout.tilesinfoactivity);
+
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);  
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		LinearLayout layout = (LinearLayout)findViewById(R.id.MainLayout);
+
+		TextView view0 = (TextView)findViewById(R.id.TextView01);
+		view0.setText(Html.fromHtml(getString(R.string.TextView01)));
+		view0.setMovementMethod(LinkMovementMethod.getInstance());
 
 		TextView view1 = addTileInfo(layout, getString(R.string.TilesAbigabaHeader), getString(R.string.TilesAbigabaInfo));
 		Pattern pattern1 = Pattern.compile("www.multifoliate.com/nh/");
@@ -43,15 +54,16 @@ public class NetHackTilesInfoActivity extends Activity
 		TextView view3 = addTileInfo(layout, getString(R.string.TilesAbsurdHeader), getString(R.string.TilesAbsurdInfo));
 		Pattern pattern2 = Pattern.compile("www.aesthetictech.8m.com/Absurd.htm");
 		Pattern pattern3 = Pattern.compile("nethack.wikia.com/wiki/The_Absurd_NetHack_Tileset");
+		Pattern pattern6 = Pattern.compile("www.thinktwicenews.com");
 		Linkify.addLinks(view3, pattern2, "http://");
 		Linkify.addLinks(view3, pattern3, "http://");
+		Linkify.addLinks(view3, pattern6, "http://");
 
 		TextView view4 = addTileInfo(layout, getString(R.string.TilesAbsurdAltHeader), getString(R.string.TilesAbsurdAltInfo));
 		Pattern pattern4 = Pattern.compile("bilious.homelinux.org/~paxed/nethack/nhsshot/tileset/absurd32_nethack.png");
 		Linkify.addLinks(view4, pattern4, "http://");
 
 		TextView view5 = addTileInfo(layout, getString(R.string.TilesRLTilesHeader), getString(R.string.TilesRLTilesInfo));
-		Pattern pattern5 = Pattern.compile("rltiles.sf.net");
-		Linkify.addLinks(view5, pattern5, "http://");
+		Linkify.addLinks(view5, Linkify.ALL);
 	}
 }
