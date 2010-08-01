67f8d8b6af789c256552c42e9d497e17f2a02ad0
farnstrom <farnstrom@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2010-08-01 06:50:49 +0000

    Removed the two tilesets for Slash'EM for now, as they won't work properly until/unless transparent tile drawing gets implemented.
    Updated the README and info screen accordingl, as well as other NetHack->Slash'EM adaptations.

--- SlashEMTilesPack1/src/com/slashemff_tiles_pack1/NetHackTilesInfoActivity.java
+++ SlashEMTilesPack1/src/com/slashemff_tiles_pack1/NetHackTilesInfoActivity.java
@@ -1,4 +1,4 @@
-package com.nethackff_tiles_pack1;
+package com.slashemff_tiles_pack1;
 
 import android.app.Activity;
 import android.graphics.Typeface;
@@ -47,23 +47,5 @@ public class NetHackTilesInfoActivity extends Activity
 		TextView view1 = addTileInfo(layout, getString(R.string.TilesAbigabaHeader), getString(R.string.TilesAbigabaInfo));
 		Pattern pattern1 = Pattern.compile("www.multifoliate.com/nh/");
 		Linkify.addLinks(view1, pattern1, "http://");
-
-		TextView view2 = addTileInfo(layout, getString(R.string.TilesAbigabaAltHeader), getString(R.string.TilesAbigabaAltInfo));
-		Linkify.addLinks(view2, pattern1, "http://");
-
-		TextView view3 = addTileInfo(layout, getString(R.string.TilesAbsurdHeader), getString(R.string.TilesAbsurdInfo));
-		Pattern pattern2 = Pattern.compile("www.aesthetictech.8m.com/Absurd.htm");
-		Pattern pattern3 = Pattern.compile("nethack.wikia.com/wiki/The_Absurd_NetHack_Tileset");
-		Pattern pattern6 = Pattern.compile("www.thinktwicenews.com");
-		Linkify.addLinks(view3, pattern2, "http://");
-		Linkify.addLinks(view3, pattern3, "http://");
-		Linkify.addLinks(view3, pattern6, "http://");
-
-		TextView view4 = addTileInfo(layout, getString(R.string.TilesAbsurdAltHeader), getString(R.string.TilesAbsurdAltInfo));
-		Pattern pattern4 = Pattern.compile("bilious.homelinux.org/~paxed/nethack/nhsshot/tileset/absurd32_nethack.png");
-		Linkify.addLinks(view4, pattern4, "http://");
-
-		TextView view5 = addTileInfo(layout, getString(R.string.TilesRLTilesHeader), getString(R.string.TilesRLTilesInfo));
-		Linkify.addLinks(view5, Linkify.ALL);
 	}
 }
