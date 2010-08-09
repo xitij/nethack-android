dc2d3d28729d1df993f9d113ec4b14f9b0df5f09
farnstrom <farnstrom@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2010-08-09 03:20:28 +0000

    Added new files (empty in this commit), for moving some of the communication code to.

---
+++ trunk/nethack-3.4.3/sys/android/NetHackNative/comm.c
@@ -0,0 +1,5 @@
+/* comm.c */
+
+#include "comm.h"
+
+/* End of file comm.c */
---
+++ trunk/nethack-3.4.3/sys/android/NetHackNative/comm.h
@@ -0,0 +1,10 @@
+/* comm.h */
+
+#ifndef ANDROID_COMM_H
+#define ANDROID_COMM_H
+
+
+
+#endif	/* ANDROID_COMM_H */
+
+/* End of file comm.h */
