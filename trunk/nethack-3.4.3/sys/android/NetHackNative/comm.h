dfec0e38f2a159b36a91a6c1f0a31a059d001222
farnstrom <farnstrom@f2e3949d-9539-bad3-cf8c-b7f925cf7ed0>
2010-08-16 00:44:39 +0000

    A little more refactoring.

--- trunk/nethack-3.4.3/sys/android/NetHackNative/comm.h
+++ trunk/nethack-3.4.3/sys/android/NetHackNative/comm.h
@@ -15,6 +15,8 @@
 extern sem_t s_ReceiveWaitingForConsumptionSema;
 extern int s_ReceiveWaitingForConsumption;
 extern pthread_mutex_t s_ReceiveMutex;
+extern int s_ReceiveWaitingForData;
+extern sem_t s_ReceiveWaitingForDataSema;
 
 void android_msgq_push_byte(unsigned char c);
 void android_msgq_init();
@@ -22,6 +24,8 @@ int android_msgq_is_empty();
 unsigned char android_msgq_pop_byte();
 void android_msgq_push_byte(unsigned char c);
 void android_msgq_wake_waiting();
+void android_msgq_begin_message(void);
+void android_msgq_end_message(void);
 
 #endif	/* ANDROID_COMM_H */
 
